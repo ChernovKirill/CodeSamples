d7e9454db5e25de663a40002b60c0e46d3cefcf1
Kirill <kgchernov@gmail.com>
2016-11-25 00:26:55 +0300

    Update numeration.cpp

--- numeration.cpp
+++ numeration.cpp
@@ -1,8 +1,17 @@
+struct UnitTG
+{
+    double x;
+    double y;
+};
+
+const int unitsNum = ; // total numbers of points
 UnitTG unitsG[unitsNum+1];
-const double step3m[3] = {1.,sqrt(3.)/2.,.5};
-const int ShiftsNum = 2+6;
-const double shifts3[ShiftsNum][3] = {{1.,0.,0.},{0.,1.,1.},{0.,1.,-1.},{-1.,0.,0.},{0.,-1.,-1.},{0.,-1.,1.},{1.,0.,0.},{0.,1.,1.}};
-const int RowNum = 6+1;
+const int HexDirections = 3;
+const double step3m[HexDirections] = {1.,sqrt(3.)/2.,.5};
+const int ShiftsNum = HexDirections * 2;
+const double shifts3[ShiftsNum][HexDirections] = {{1.,0.,0.},{0.,1.,1.},{0.,1.,-1.},{-1.,0.,0.},{0.,-1.,-1.},{0.,-1.,1.}};
+const int RowNum = ; // number of layers (must be calculated by unitsNum)
+const int step = ; // distance between points
 
 i=1;
 unitsG[i].x = 0.;
@@ -16,7 +25,7 @@ for (int j = 1; j <= RowNum; j++) {
     unitsG[i].y += rCount == 0 ? 0. : 1. * step * (step3m[0] * shifts3[1][0] + step3m[2] * shifts3[1][2]);
     rCount++;
     int k;
-    for (k = 2; k < ShiftsNum - 1; k++) {
+    for (k = 2; k <= ShiftsNum; k++) {
         for (int l = 0; l < rCount; l++) {
             i++;
             unitsG[i].x = unitsG[i-1].x + step * step3m[1] * shifts3[k][1];
